4d6bc2462d35c9065079f33e2b957de5f74af7d7
Md Abid Hassan Mitul <154643934+mitul-abid@users.noreply.github.com>
2025-06-30 18:25:56 +0600

    bandNameGenerator.py
    Project 1: Band Name Generator

---
+++ bandNameGenerator.py
@@ -0,0 +1,8 @@
+#Project 1 - Band Name Generator
+
+print("Welcome to the Band Name Generator.")
+
+city = input("Which city did you grow up in? \n")
+pet = input("What is the name of your favorite pet? \n")
+
+print("Your Band Name could be: " + city + " " + pet)
